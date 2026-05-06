4a3ae5bc11b877b1e148c3296847bc67193d7296
Luis <engs-luiskomar@camporeal.edu.br>
2026-05-05 22:06:19 -0300

    acabo

--- script.js
+++ script.js
@@ -1,51 +1,80 @@
 let lista_nomes = ["Mago", "Magro", "seco"]
+let lista_filtrada = lista_nomes.filter(nome => nome.length >= 5)
 
-function renderizar() { 
-     const lista_pagina = document.getElementById("lista")
-     lista_pagina.innerHTML = ""
-      for(let item of lista_nomes) {
-      let elemento = document.createElement("li")
-      elemento.innerText = item
-      lista_pagina.appendChild(elemento)
-    
-    }
-    
- }
+function renderizar() {
+   const lista_pagina = document.getElementById("lista")
+   const lista_pagina_m = document.getElementById("lista_maiusculo")
 
- document.addEventListener("DOMContentLoaded", () => {
-    renderizar()
- })
+   lista_pagina.innerHTML = ""
+  lista_nomes.forEach(nome => {
+   let li = document.createElement("li")
+   li.innerText = nome
+   lista_pagina.appendChild(li)
+  })
+   contarnome()
+}
 
- function contarNomesGrandes(){
-   let contador = 0
+document.addEventListener("DOMContentLoaded", () => {
+   renderizar()
+})
 
-   for(let nome  of lista_nomes) {
-      if (nome.length > 5) {
-         contador ++
+function contarnome() {
+   const contagem = document.getElementById("contagem")
+   let contador = 0;
+
+   for (let nome of lista_nomes) {
+      if (nome.length >= 5) {
+         contador++
       }
+      contagem.innerText = contador;
    }
-   return contador
- }
- document.getElementById("add_final") .addEventListener("click", () => {
+
+}
+
+
+document.getElementById("add_final").addEventListener("click", () => {
    const input = document.getElementById("inputnome")
    lista_nomes.push(input.value)
    input.value = ""
    renderizar()
- })
+})
 
- document.getElementById("rem_ultimo").addEventListener("click", () => {
-    lista_nomes.pop()
-    renderizar()
+document.getElementById("rem_ultimo").addEventListener("click", () => {
+   lista_nomes.pop()
+   renderizar()
 })
 
 document.getElementById("rem_primeiro").addEventListener("click", () => {
-    lista_nomes.shift()
-    renderizar()
+   lista_nomes.shift()
+   renderizar()
 })
 
 document.getElementById("add_inicio").addEventListener("click", () => {
-    const input = document.getElementById("inputnome")
-    lista_nomes.unshift(input.value)
-    input.value = ""
-    renderizar()
+   const input = document.getElementById("inputnome")
+   lista_nomes.unshift(input.value)
+   input.value = ""
+   renderizar()
 })
+
+document.getElementById("maiusculo").addEventListener("click", () => {
+   let lista_maiusculo = lista_nomes.map(nome => nome.toUpperCase())
+   renderizar2(lista_maiusculo)
+})
+
+document.getElementById("filtrar").addEventListener("click", () => {
+   renderizar2("lista_filtrada")
+})
+
+
+
+function renderizar2(lista_qualquer){
+const lista_nova = document.getElementById("lista_2")
+
+lista_nova.innerHTML = ""
+
+lista_qualquer.forEach(nome => {
+   let li = document.createElement("li")
+   li.innerText = nome
+   lista_nova.appendChild(li)
+})
+}
